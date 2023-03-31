abafca7d482ecc017cdef4c7dd61cadff59256b9
ShuranZhang <szhang73@u.rochester.edu>
2023-03-31 21:56:31 +0000

    DBZ-6262 Enable pass through internal configs for sync topic
    DBZ-6262 Enable pass through internal configs for sync topic
    
    enable compression type for sync topic config
    
    DBZ-6178 Fix mistake not using the passed index
    
    fix compression type config description
    
    enable the user to configure arbitrary sync topic configs through sync topic internal prefix
    
    remove unnecessary field for sync topic config prefix

--- src/main/java/io/debezium/connector/spanner/config/BaseSpannerConnectorConfig.java
+++ src/main/java/io/debezium/connector/spanner/config/BaseSpannerConnectorConfig.java
@@ -71,6 +71,7 @@ public abstract class BaseSpannerConnectorConfig extends CommonConnectorConfig {
     private static final String CONNECTOR_SPANNER_SYNC_SEGMENT_MS_POLICY_PROPERTY_NAME = "connector.spanner.sync.segment.ms";
     private static final String CONNECTOR_SPANNER_SYNC_MIN_CLEANABLE_DIRTY_RATIO_PROPERTY_NAME = "connector.spanner.sync.min.cleanable.dirty.ratio";
     private static final String CONNECTOR_SPANNER_SYNC_KAFKA_BOOTSTRAP_SERVERS_PROPERTY_NAME = "connector.spanner.sync.kafka.bootstrap.servers";
+    public static final String CONNECTOR_SPANNER_SYNC_TOPIC_INTERNAL_PREFIX = "connector.spanner.sync.topic.internal.";
 
     private static final String CONNECTOR_SPANNER_PARTITION_FINISHING_AFTER_COMMIT_PROPERTY_NAME = "connector.spanner.partition.finishing.afterCommit";
 
