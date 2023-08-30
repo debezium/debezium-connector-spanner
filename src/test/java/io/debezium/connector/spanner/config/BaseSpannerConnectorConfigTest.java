bb71998f4560be29b8e512dd13683dec4ad2ebbc
ShuranZhang <szhang73@u.rochester.edu>
2023-08-30 20:35:15 +0000

    DBZ-6845 Update number of expected config properties in tests.

--- src/test/java/io/debezium/connector/spanner/config/BaseSpannerConnectorConfigTest.java
+++ src/test/java/io/debezium/connector/spanner/config/BaseSpannerConnectorConfigTest.java
@@ -32,7 +32,7 @@ class BaseSpannerConnectorConfigTest {
     void testConfigDef() {
         ConfigDef actualConfigDefResult = BaseSpannerConnectorConfig.configDef();
         Map<String, ConfigDef.ConfigKey> configKeysResult = actualConfigDefResult.configKeys();
-        assertEquals(56, configKeysResult.size());
+        assertEquals(57, configKeysResult.size());
         List<String> groupsResult = actualConfigDefResult.groups();
         assertEquals(3, groupsResult.size());
         assertEquals("Spanner", groupsResult.get(0));
