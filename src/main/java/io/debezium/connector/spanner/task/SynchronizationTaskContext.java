ce668f06ef5a24469da63b23163a1dd1f84f6c15
nancyxu123 <nancyxu567@gmail.com>
2023-10-02 21:13:09 +0000

    DBZ-6794 Remove commented out code

--- src/main/java/io/debezium/connector/spanner/task/SynchronizationTaskContext.java
+++ src/main/java/io/debezium/connector/spanner/task/SynchronizationTaskContext.java
@@ -264,16 +264,6 @@ public class SynchronizationTaskContext {
     private void onError(Throwable throwable) {
         LOGGER.info("Task {}, enqueueing error in task", this.taskSyncContextHolder.get().getTaskUid(), throwable);
         this.errorHandler.setProducerThrowable(throwable);
-        // if (this.rebalancingEventListener != null) {
-        // try {
-        // LOGGER.info("Task {}, shutting down rebalancing event listener due to error in task", this.taskSyncContextHolder.get().getTaskUid(), throwable);
-        // this.rebalancingEventListener.shutdown();
-        // }
-        // catch (Exception e) {
-        // LOGGER.info("Task {}, caught exception when shutting down rebalancing event listener due to error in task", this.taskSyncContextHolder.get().getTaskUid(),
-        // e);
-        // }
-        // }
     }
 
     private void onFinish() {
