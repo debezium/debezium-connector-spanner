0be4a229f17cf20f2798b5ccba18b3aad7e367ac
Fiore Mario Vitale <mvitale@redhat.com>
2025-12-05 12:24:15 +0100

    debezium/dbz#11 Migrate to JUnit5

--- src/test/java/io/debezium/connector/spanner/LowWatermarkRecordIT.java
+++ src/test/java/io/debezium/connector/spanner/LowWatermarkRecordIT.java
@@ -6,7 +6,7 @@
 package io.debezium.connector.spanner;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.time.Instant;
 import java.time.format.DateTimeFormatter;
